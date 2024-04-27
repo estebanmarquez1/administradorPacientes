a8829cae98337ef969f4d04f4a0971dc676b5f9f
Esteban Márquez <marquezriosesteban@gmail.com>
2024-04-27 14:43:50 -0600

    Some changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
     return(
         <>
-        <h1 className="font-black text-4xl text-center md:w-2/3 mx-auto">Seguimiento Pacientes{" "}<span className="text-indigo-600">Consultorio ejemplo</span></h1>
+        <h1 className="font-black text-4xl text-center md:w-2/3 mx-auto">Seguimiento Pacientes{" "}<span className="text-indigo-600">Consultorio</span></h1>
         <p className="text-center mt-3 mb-7">Añade tus pacientes y {''} <span className="text-indigo-600 font-bold">adminístralos</span></p>
         </>
         )
